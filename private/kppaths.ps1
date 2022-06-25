40c9785cc5a8f730771f2cd43c98091fea186d9d
Cramer McGarr <54294273+CRAYPAUL@users.noreply.github.com>
2022-06-24 21:51:34 -0500

    Add files via upload

---
+++ private/kppaths.ps1
@@ -0,0 +1,13 @@
+﻿#Defines local DB path
+Function Import-VaultLocation {
+    $DBPaths = "$env:USERPROFILE\", "$env:OneDrive\", "$env:ProgramW6432\", "${env:ProgramFiles(x86)}\"
+    foreach ($DBPath in $DBPaths) {
+	    $FindDB = Get-ChildItem -Path "$DBPath*" -Include *.kdbx -Recurse -ErrorAction SilentlyContinue
+	    $DBCount = $FindDB.Count
+
+	    if ($DBCount -eq "1") {
+		    $global:VaultDB = $FindDB.FullName
+		    break
+	    }
+    }
+}
